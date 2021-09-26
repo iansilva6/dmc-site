b1e1fbdf38c5f0dcd2d83541453e39a85c19ef34
Ian Silva <iansilva6@yahoo.com.br>
2021-09-26 09:06:36 -0300

    feat: improve seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,7 +31,7 @@ export default class MyDocument extends Document {
     }
     render() {
         return (
-            <Html>
+            <Html lang="pt-br">
                 <Head>
                 <link
                     href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap"
